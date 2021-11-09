42a4cfc871f282a6fa9d57febbb1ea786441d0e0
anilnamdev1989 <anilnamdev10@gmail.com>
2021-11-09 18:29:04 +0530

    hii

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your roll no.");
+            int roll = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("enter your emailid");
+            string email = Console.ReadLine();
+            Console.WriteLine("enter your address");
+            string address = Console.ReadLine();
+            StudentModel oStudentModel = new StudentModel();
+            oStudentModel.StudentRegistration(roll, name, email, address);
+        }
+    }
+}
---
+++ ConsoleApp3/StudentModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class StudentModel
+    {
+        public string StudentRegistration(int roll, string name, string email,string address)
+        {
+            string dbConnection = "Data Source=ServerName, Intial Catalog=Databasename";
+            SqlConnection scn = new SqlConnection(dbConnection);
+            scn.Open();
+            SqlCommand cmd = new SqlCommand("prc_product", scn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@name", name);
+            cmd.Parameters.AddWithValue("@email", email);
+            cmd.Parameters.AddWithValue("@address", address);
+            cmd.Parameters.AddWithValue("@roll", roll);
+            int recordinserted = cmd.ExecuteNonQuery();
+            return "Student record created";
+        }
+    }
+}
